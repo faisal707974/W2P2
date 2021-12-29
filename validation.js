5788aa8dff9b893554fa588e76591a140b0c2911
Faisal707974 <faisal707974@gmail.com>
2021-12-29 13:28:57 +0530

    form validated

--- validation.js
+++ validation.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#msg-form").validate({
+    $("#gform").validate({
         rules:{
             subject:{
                 minlength:4
@@ -11,8 +11,91 @@ $(document).ready(function(){
         },
         messages:{
             name:{
-                minlength:"Characters should be more than 8 and less than 10"
+                minlength:"Characters should between 8 and 10"
             }
+        }
+    })
+})
+
+
+function validation(){
+    let name = document.forms['msgform']['name'].value;
+    let email = document.forms['msgform']['email'].value;
+    let subject = document.forms['msgform']['subject'].value;
+    let message = document.forms['msgform']['message'].value;
+    let contact = document.forms['msgform']['contact'].value;
+    let contactLength = contact.length;
+
+    if(name == ''){
+        document.getElementById('name').style.borderColor='red';
+    }
+    else{
+        document.getElementById('name').style.borderColor='black';
+    }
+    if(email == ''){
+        document.getElementById('email').style.borderColor='red';
+    }
+    if(subject == ''){
+        document.getElementById('subject').style.borderColor='red';
+    }
+    if(message == ''){
+        document.getElementById('message').style.borderColor='red';
+    }
+    if(contact == ''){
+        document.getElementById('mycontact').style.borderColor='red';
+    }
+    if(contactLength<10 || contactLength>10){
+        document.getElementById('mycontact').style.borderColor='red';
+        document.getElementById('lessthan10').style.display = 'block';
+        document.getElementById('lessthan10').innerHTML = 'Enter 10 digits';
+    }
+    
+}
+
+function checknum(event){
+    let x = event.which;
+
+    document.getElementById('test').innerHTML = x;
+
+    if(x<48 || x>57){
+        document.getElementById('lessthan10').style.display = 'block';
+        document.getElementById('lessthan10').style.color = 'red';
+        document.getElementById('lessthan10').innerHTML = 'Number only';
+    }else{
+        document.getElementById('lessthan10').style.display = 'none';
+        document.getElementById('lessthan10').style.color = 'black';
+    }
+}
+
+function checkalph(event){
+    let x = event.which;
+
+    document.getElementById('test').innerHTML = x;
+
+    if( x<97 || x>122){
+        document.getElementById('alpherror').style.display = 'block';
+        document.getElementById('alpherror').style.color = 'red';
+        document.getElementById('name').style.borderColor = 'red';
+        document.getElementById('alpherror').innerHTML = 'Alphabets only';
+    }else{
+        document.getElementById('alpherror').style.display = 'none';
+    }
+}
+
+$("#submit-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycbxpX8Muq5pTVNh47Df4EmSUIyaaJLntji66xLtghfOPX3mProzwZv2-DqKELdCMRGZt/exec",
+        data:$("#submit-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error")
+
         }
     })
 })
